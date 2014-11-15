20f1172f2a8b766aef50fdfdb147cff1b0639b36
Tyler Yasaka <web@tyleryasaka.me>
2014-11-14 21:35:35 -0600

    Cleaned code

--- graph.h
+++ graph.h
@@ -48,58 +48,35 @@ graph::graph(int graph_size){
 
 //input: indeces of nodes in matrix for begin and end point of desired path
 //output: for now, array of indeces could be set to object's "path" array for shortest path... maybe change to LinkedList?
-stack<int> graph::ShortestPath(int A,int B){
-	int eye = A;  
-	node* eyeNode = &nodes[eye];
-	int end = B;
-	
-	eyeNode->cost = 0;
-	
+stack<int> graph::ShortestPath(int start,int end){
+	start->cost = 0;//Setting start cost to 0 because: (1.) logically makes sense (2.) will keep final cost from being off by one (3.) marks the origin conveniently
+	int eyeball = start;//Our eye starts at the start point of course
 	heap->Insert(eyeNode);
-	stack<int> path;
-
-	while(!heap->empty() && &(nodes[end]) != heap->Min()){             //root method will take a node as a parameter and determine if that node is the root
-			eyeNode = heap->Remove();              
-		eye = eyeNode->location;
-		cout<<eye<<endl; 	
-													//if end holds the smallest value in heap you know you reached the shortest path because 											//all other nodes are further from the orgin
-		for(int k=0; k < size; k++){
-			if(matrix[eye][k] != -1){      //if there's a connection from the eye to the indexed node. Two if statements are for clarity
-				if(nodes[eye].cost + matrix[eye][k] < nodes[k].cost || nodes[k].cost == -1){   //if the eye's distance from orgin + endgelength  less than what the node currently costs
-						nodes[k].cost = nodes[eye].cost + matrix[eye][k];
-						nodes[k].prev = eyeNode;
-						cout<<eye<<" is connected to "<<nodes[k].location<<endl;
-						heap->Insert(&nodes[k]);
+	stack<int> path;//This is where we'll store the final path
+	while(!heap->empty() && &(nodes[end]) != heap->Min()){//min method will take a node as a parameter and determine if that node is the root
+		
+		nodes[eyeball] = heap->Remove();//go to next item in the heap, and pop it off the heap    
+		eye = nodes[eyeball]->location;//this is the matrix location of our new node
+		for(int k=0; k < size; k++){//Iterate through the matrix to find any connections
+			
+			if(matrix[eye][k] != -1){//if there's a connection from the eye to the indexed node. Two if statements are for clarity
+				if(nodes[eyeball].cost + matrix[eyeball][k] < nodes[k].cost || nodes[k].cost == -1){//if the eye's distance from orgin + endgelength  less than what the node currently costs
+					
+						nodes[k].cost = nodes[eyeball].cost + matrix[eyeball][k];//Change cost of eye node to new total cost
+						nodes[k].prev = nodes[eyeball];//So eye node will know who changed it
+						heap->Insert(&nodes[k]);//Add this to our heap to become our eye at a later point
 				}
 			}
 		}
-		 //this should change to 1 instead it stays at 0
-
 	}
 	
-	
-	if(nodes[end].cost == -1){
-		cout<<"Path is not connected";
-	}
-	else{
-		cout << "path: " << endl;
-		int current = end;
-		while(nodes[current].cost != 0){
-			path.push(current);
-			current = nodes[current].prev->location;
-			cout << current << endl;
-		}
-		 
-		
+	int current = end;//Now we will trace path from end to beginning
+	while(current != start){//when current is beginning
+		path.push(current);
+		current = nodes[current].prev->location;
 	}
 	
-
-
-	
-	cout<<endl;
-	
-	
-	return path;
+	return path;//Replace this.
 
 		
 }
