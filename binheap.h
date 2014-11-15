958115549d4f553b14a72360cdee6e8398e86b0a
Tyler Yasaka <web@tyleryasaka.me>
2014-11-14 18:21:07 -0600

    Implemented remove for Binary Heap. 	modified:   binheap.h

--- binheap.h
+++ binheap.h
@@ -14,7 +14,7 @@ class binheap {
 		binheap():heap(0),size(0),last(-1){};
 		binheap(int heap_size);
         void Insert(node* n);
-        void Remove(node* n);
+        node* Remove();
         node* Min()const;
 };
 
@@ -61,6 +61,46 @@ void binheap::Insert(node* n){
 		}
 		else//item is greater than or equal to parent (what we want)
 			done = true;//fall out of the loop
+	}
+}
+
+node* binheap::Remove(){
+	if(last>=0){
+		bool done = false;
+		node* out = heap[0];//pass node pointer to variable to be returned
+		heap[0] = heap[last];//Place last item at top of heap
+		heap[last] = 0;//Remove the last item from former location
+		last--;//Now last item is one lower in heap
+		int current = 0;//keeps track of the location of node we are moving
+		int left = 0;//var for left child
+		int right = 0;//var for right child
+		int minchild = 0;//temp variable for comparing children
+		while(!done){
+			left = current*2+1;
+			right = current*2+2;
+			if(left <= last){//if it has a left child
+				if(right <= last){//if it has a right child
+					if(heap[left]->cost < heap[right]->cost)//find which child is smaller
+						minchild = left;
+					else
+						minchild = right;
+					if(heap[minchild]->cost < heap[current]->cost){//compare current with smallest child
+						SwapNode(current,minchild);
+						current = minchild;
+					}
+					else//if smaller child is not smaller than current, we're done
+						done = true;
+				}
+				else if(heap[left]->cost < heap[current]->cost){//if left less than current, swap
+					SwapNode(current, left);
+					current = left;//our current node is now where left was
+				}
+				else
+					done = true;//if left but no right and left not less than current, we're done
+			}
+			else//if no left child, can't have right child, so we're done
+				done = true;
+		}
 	}
 }
 
