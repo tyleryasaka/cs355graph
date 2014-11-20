41fc5834c7a5f60de1e9523f51d2b310674a569d
mtuck <Mtuck@una.edu>
2014-11-19 21:08:13 -0600

    added dummy checks

--- graph.h
+++ graph.h
@@ -32,7 +32,7 @@ void graph::Copy(const graph& source){
 	nodes = new node[size];
 	matrix = new int*[size];
 	for(int i=0;i<size;i++){
-		nodes[i] = source.nodes[i];
+		nodes[i] = source.nodes[i]; 
 		matrix[i] = source.matrix[i];
 	}
 	heap = new binheap(size);
@@ -88,59 +88,59 @@ graph::~graph(){
 //input: indeces of nodes in matrix for begin and end point of desired path
 //output: for now, array of indeces could be set to object's "path" array for shortest path... maybe change to LinkedList?
 path graph::ShortestPath(int start,int end){
-	nodes[start].cost = 0;//Setting start cost to 0 because: (1.) logically makes sense (2.) will keep final cost from being off by one (3.) marks the origin conveniently
-	node* eyeball = &nodes[start];//Our eye starts at the start point of course
-	heap->Insert(eyeball);
-	while(!heap->empty() && &(nodes[end]) != heap->Min()){//min method will take a node as a parameter and determine if that node is the root
-		
-		eyeball = heap->Remove();//go to next item in the heap, and pop it off the heap    
-		for(int k=0; k < size; k++){//Iterate through the matrix to find any connections
+	path returnPath;  //moved to top for return statement
+	if(start < size && start >= 0 && end < size && end >= 0){
+		nodes[start].cost = 0;//Setting start cost to 0 because: (1.) logically makes sense (2.) will keep final cost from being off by one (3.) marks the origin conveniently
+		node* eyeball = &nodes[start];//Our eye starts at the start point of course
+		heap->Insert(eyeball);
+		while(!heap->empty() && &(nodes[end]) != heap->Min()){//min method will take a node as a parameter and determine if that node is the root
 			
-			if(matrix[eyeball->location][k] != -1){//if there's a connection from the eye to the indexed node. Two if statements are for clarity
-				if(nodes[eyeball->location].cost + matrix[eyeball->location][k] < nodes[k].cost || nodes[k].cost == -1){//if the eye's distance from orgin + endgelength  less than what the node currently costs
-					
-						nodes[k].cost = nodes[eyeball->location].cost + matrix[eyeball->location][k];//Change cost of eye node to new total cost
-						nodes[k].prev = eyeball;//So eye node will know who changed it
-						heap->Insert(&nodes[k]);//Add this to our heap to become our eye at a later point
+			eyeball = heap->Remove();//go to next item in the heap, and pop it off the heap    
+			for(int k=0; k < size; k++){//Iterate through the matrix to find any connections
+				
+				if(matrix[eyeball->location][k] != -1){//if there's a connection from the eye to the indexed node. Two if statements are for clarity
+					if(nodes[eyeball->location].cost + matrix[eyeball->location][k] < nodes[k].cost || nodes[k].cost == -1){//if the eye's distance from orgin + endgelength  less than what the node currently costs
+						
+							nodes[k].cost = nodes[eyeball->location].cost + matrix[eyeball->location][k];//Change cost of eye node to new total cost
+							nodes[k].prev = eyeball;//So eye node will know who changed it
+							heap->Insert(&nodes[k]);//Add this to our heap to become our eye at a later point
+					}
 				}
 			}
 		}
-	}
-	
-	int current = end;//Now we will trace path from end to beginning
-	
-	path returnPath;
-	
-	//make sure a path was actually found
-	if(nodes[current].prev!=0){
-		int* backtrack = new int[size];//This is where we'll store the final path
-		int pathlen = 0;
-		while(current != start){//when current is beginning
-			backtrack[pathlen] = current;
+		
+		int current = end;//Now we will trace path from end to beginning
+		
+		//make sure a path was actually found
+		if(nodes[current].prev!=0){
+			int* backtrack = new int[size];//This is where we'll store the final path
+			int pathlen = 0;
+			while(current != start){//when current is beginning
+				backtrack[pathlen] = current;
+				pathlen++;
+				current = nodes[current].prev->location;
+			}
+			backtrack[pathlen] = start;//Don't forget to include the first node!
 			pathlen++;
-			current = nodes[current].prev->location;
+		
+			path successPath(pathlen);
+			returnPath = successPath;
+			for(int i=0;i<pathlen;i++){
+				returnPath.location[i] = backtrack[(pathlen-i-1)];
+			}
 		}
-		backtrack[pathlen] = start;//Don't forget to include the first node!
-		pathlen++;
-	
-		path successPath(pathlen);
-		returnPath = successPath;
-		for(int i=0;i<pathlen;i++){
-			returnPath.location[i] = backtrack[(pathlen-i-1)];
+		else{
+			path failPath(0);
+			returnPath = failPath;
 		}
+		
+		ClearPath();//Clear the path made from the graph
 	}
-	else{
-		path failPath(0);
-		returnPath = failPath;
-	}
-	
-	ClearPath();//Clear the path made from the graph
-	
 	return returnPath;
 }
 
 void graph::InsertEdge(int from, int to, int weight){
-	if(from >= 0 && from < size && to >= 0 && to < size){
+	if(from >= 0 && from < size && to >= 0 && to < size && weight > 0){
 		matrix[from][to] = weight;
 	}
 }
